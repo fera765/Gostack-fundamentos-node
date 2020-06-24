199df02f48c5ddb94915017ead4b43af984012d0
Mateus Conceição <fera7775@yahoo.com>
2020-06-24 16:10:59 -0300

    Fundamentos de nodejs

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,7 +14,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
+  public execute({ title, value, type }: Request): Transaction {
+    const fundsValidator = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && fundsValidator.total < value) {
+      throw Error('Insufficient funds');
+    } else if (type !== 'income' && type !== 'outcome') {
+      throw Error('declined transaction, invalid type');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
     // TODO
   }
 }
